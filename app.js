13e559fd1db8f17833e47788712b5008ab1a9596
mahteusodev <mahteusodev@gmail.com>
2022-08-17 01:09:48 -0300

    added styles

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const btn = document.querySelector('.btn');
-const h1 = document.querySelector('#text');
-
+let btn = document.querySelector('#btn');
 
 btn.addEventListener('click', function(){
     let r = Math.floor(Math.random() * 255) + 1;
@@ -8,5 +6,17 @@ btn.addEventListener('click', function(){
     let b = Math.floor(Math.random() * 255) + 1;
  
     document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    document.h1.innerText = `rgb(${r}, ${g}, ${b})`;
+    document.getElementById('text').innerHTML = `rgb(${r}, ${g}, ${b})`;
+
+    if (r < 100 && g < 100 && b < 100){
+        document.getElementById('text').style.color = '#FFF';
+        document.getElementById('btn').style.color = '#FFF';
+        document.getElementById('btn').style.borderColor = '#FFF';
+        document.getElementById('creator').style.color = '#FFF';
+    } else {
+        document.getElementById('text').style.color = '#000';
+        document.getElementById('btn').style.color = '#000';
+        document.getElementById('btn').style.borderColor = '#000';
+        document.getElementById('creator').style.color = '#000';
+    }
 })
